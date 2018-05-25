7a3e44d8ebab18d5bc3dc95b450018d8a078c461
sachin <sachin@empressinfotech.com>
2018-05-25 20:13:53 +0530

    Moviz first commit

---
+++ moviez/Scripts/moviz.js
@@ -0,0 +1,8 @@
+﻿var showSignupDiv = function () {
+    $("#loginContent").hide();
+    $("#signUpContent").show();
+};
+var showLoginDiv = function () {
+    $("#signUpContent").hide()
+    $("#loginContent").show();
+};
